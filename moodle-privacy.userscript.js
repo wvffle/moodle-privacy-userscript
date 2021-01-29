3efcfd6ae25f30cccdde0a8bc5e749459b37c5f5
Kasper Seweryn <github@wvffle.net>
2021-01-29 11:52:54 +0000

    Create moodle-privacy.userscript.js

---
+++ moodle-privacy.userscript.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name     Moodle Privacy UserScript
+// @version  1.0.0
+// @run-at 	 document-start
+// @include  /^https?://cez(2|e)?\.wi\.pb\.edu\.pl/.*$/
+// @grant    none
+// ==/UserScript==
+
+const events = ['visibilitychange', 'webkitvisibilitychange', 'blur', 'focus']
+
+for (const target of [window, document]) {
+  for (const event of events) {
+    target.addEventListener(event, e => e.stopImmediatePropagation(), true)
+  }
+}
