1c52320370f65e3779b53798a5b789e5d35aedd4
Kasper Seweryn <github@wvffle.net>
2021-01-29 12:24:08 +0000

    Update moodle-privacy.user.js

--- moodle-privacy.user.js
+++ moodle-privacy.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Moodle Privacy UserScript
-// @version  1.1.0
+// @version  1.2.0
 // @run-at   document-start
 // @include  /^https?://cez(2|e)?\.wi\.pb\.edu\.pl/.*$/
 // @grant    none
